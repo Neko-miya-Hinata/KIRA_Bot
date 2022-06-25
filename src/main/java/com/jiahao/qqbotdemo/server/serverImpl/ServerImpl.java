f0b9511513cc7f8c011624f656fa6be1c232521b
后家豪 <819382605@qq.com>
2022-06-25 19:02:48 +0800

    添加每日早报定时任务

--- src/main/java/com/jiahao/qqbotdemo/server/serverImpl/ServerImpl.java
+++ src/main/java/com/jiahao/qqbotdemo/server/serverImpl/ServerImpl.java
@@ -3,6 +3,7 @@ package com.jiahao.qqbotdemo.server.serverImpl;
 import com.jiahao.qqbotdemo.entry.Message;
 import com.jiahao.qqbotdemo.server.Server;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,8 +18,17 @@ public class ServerImpl implements Server {
 
     @Override
     public Object receiveMessage(Message message) {
+
         return null;
     }
 
 
+    /**
+     *  每日新闻, 9点执行
+     * */
+    @Scheduled(cron = "0 0 9 * * ?")
+    private void sendDailyHot(){
+        String url = "http://0.0.0.0:5700/send_msg?message_type=group&group_id=629472976&message=[CQ:image,file=https://api.vvhan.com/api/60s]";
+        restTemplate.getForEntity(url, String.class);
+    }
 }
--- src/test/java/com/jiahao/qqbotdemo/QqBotDemoApplicationTests.java
+++ src/test/java/com/jiahao/qqbotdemo/QqBotDemoApplicationTests.java
@@ -2,9 +2,6 @@ package com.jiahao.qqbotdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
